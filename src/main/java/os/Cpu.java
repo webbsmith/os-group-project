1c06390a34b4ed987c0328791b75b061159f7630
Webb Smith <webbsemail@gmail.com>
2018-03-12 04:36:05 +0100

    added CPU dma controller

--- src/main/java/os/Cpu.java
+++ src/main/java/os/Cpu.java
@@ -3,6 +3,29 @@ package os;
 import java.util.Queue;
 
 public class Cpu {
-    public Cpu(Queue<Program> programQueue) {
+    private final CpuDmaController cpuDmaController;
+
+    private boolean busControl = true;
+
+    public Cpu(Queue<Program> programQueue, CpuDmaController cpuDmaController) {
+        this.cpuDmaController = cpuDmaController;
+    }
+
+    public void read(int memoryAddressOfPhysicalData, int memoryAddressOfBuffer) {
+        while (!busControl) {
+            // wait
+        }
+        busControl = false;
+        cpuDmaController.read(memoryAddressOfPhysicalData, memoryAddressOfBuffer);
+        busControl = true;
+    }
+
+    public void write(int memoryAddressOfPhysicalData, int memoryAddressOfBuffer) {
+        while (!busControl) {
+            // wait
+        }
+        busControl = false;
+        cpuDmaController.write(memoryAddressOfPhysicalData, memoryAddressOfBuffer);
+        busControl = true;
     }
 }
