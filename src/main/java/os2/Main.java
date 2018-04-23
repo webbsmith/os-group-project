958a724fa763679ce56e9b6d4011271ca2f8f4ba
webbsmith <webbsemail@gmail.com>
2018-04-23 11:06:05 -0400

    progress.

--- src/main/java/os2/Main.java
+++ src/main/java/os2/Main.java
@@ -4,15 +4,13 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class Main {
-    public static final int CPU_COUNT = 1;
+    public static final int CPU_COUNT = 2;
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
         long startTime = System.currentTimeMillis();
         Driver2 driver = new Driver2();
         driver.run();
         long timeToComplete = System.currentTimeMillis() - startTime;
-        log.info("Waiting");
-        Thread.sleep(1000); // Wait for all threads to finish
         log.info("Finished all programs in {} ms", timeToComplete);
     }
 }
